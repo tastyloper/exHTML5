142ccba50d1b70f47578d210600e61660b01d808
tastyloper <tastyloper@gmail.com>
2019-04-10 10:04:56 +0900

    0410 study

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var menu = $('.menu');
+var item = $('.menu-item');
+var tab = $('.tab');
+
+// 메인 메뉴의 하위 메뉴 제어를 위한 스크립트
+menu.on('mouseout', function() {
+  item.removeClass('menu-act');
+});
+item.on('mouseover focusin', function() {
+  item.removeClass('menu-act');
+  $(this).addClass('menu-act');
+});
+
+tab.on('click keyup', function(e) {
+  e.preventDefault();
+  if (e.keyCode === 13 || e.keyCode === 32 || e.type === 'click') {
+    $(this).parent().siblings('.board-act').removeClass('board-act');
+    $(this).parent().addClass('board-act');
+  }
+});
+$('.board-tab').on('click keyup', function(e) {
+  e.preventDefault();
+  if (e.keyCode === 13 || e.keyCode === 32 || e.type === 'click') {
+    $(this).siblings('.board-act').removeClass('board-act').attr('aria-selected', false);
+    $(this).addClass('board-act').attr('aria-selected', true);
+    $('.board3').find('.tabpanel-act').removeClass('tabpanel-act');
+    $('#' + $(this).attr('aria-controls')).addClass('tabpanel-act');
+  }
+});
